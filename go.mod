b554b7b2fc99d27867d9726ef52aefad2c979796
Scott Hsieh <doodykimo@gmail.com>
2021-08-30 22:55:24 +0800

    First working prototype

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/doody/etcd_file_syncer
 
 go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/sirupsen/logrus v1.6.0
+	go.etcd.io/etcd/client/v3 v3.5.0
+)
---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+const (
+	dialTimeout    = 5 * time.Second
+	requestTimeout = 10 * time.Second
+)
+
+var (
+	etcdClient *clientv3.Client
+)
+
+// HTTP POST Model - /putFile
+type FileModel struct {
+	ETCDKey  string `json:"etcdKey"`
+	FilePath string `json:"filePath"`
+}
+
+func main() {
+	// ETCD Connection
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"192.168.1.120:2379"},
+		DialTimeout: dialTimeout,
+	})
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("error connecting to ETCD")
+
+	}
+	etcdClient = cli
+	defer cli.Close()
+
+	// ETCD Testing
+	readKeyAndSaveToFolder("", "./etcd_files")
+	go watchKeyAndSaveToFile("", "./etcd_files")
+
+	// HTTP server
+	r := gin.Default()
+	// Manual update file
+	r.POST("/putFile", func(c *gin.Context) {
+		var json FileModel
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		if err := putFileToETCD(json.ETCDKey, json.FilePath); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+	// Manual download files
+	r.POST("/downloadFile", func(c *gin.Context) {
+		var json FileModel
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		if err := readKeyAndSaveToFolder(json.ETCDKey, json.FilePath); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:3000
+}
+
+// putFileToETCD will read filePath into string and write into ETCD using etcdKey
+func putFileToETCD(etcdKey, filePath string) (err error) {
+	// Reading file
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"filePath": filePath,
+			"err":      err,
+		}).Error("error loading file")
+		return err
+	}
+
+	// Write to ETCD
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	_, err = etcdClient.Put(ctx, etcdKey, string(fileContent))
+	cancel()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"etcdKey":     etcdKey,
+			"err":         err,
+			"fileContent": string(fileContent),
+		}).Error("error putting data to ETCD")
+		return err
+	}
+	return nil
+}
+
+// watchKeyAndSaveToFile will keep watching keys in ETCD and save relative file to fileFolder
+func watchKeyAndSaveToFile(etcdKey, fileFolder string) (err error) {
+	rch := etcdClient.Watch(context.Background(), etcdKey, clientv3.WithPrefix())
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			log.WithFields(log.Fields{
+				"eventType": ev.Type,
+				"etcdKey":   ev.Kv.Key,
+			}).Info("ETCD file changed")
+			filePath := filepath.Join(fileFolder, string(ev.Kv.Key))
+			switch ev.Type {
+			case clientv3.EventTypeDelete:
+				if err := os.Remove(filePath); err != nil {
+					log.WithFields(log.Fields{
+						"filePath": filePath,
+						"err":      err,
+					}).Error("cannot delete file")
+					return err
+				}
+			case clientv3.EventTypePut:
+				saveToFolder(filePath, ev.Kv.Value)
+			}
+		}
+	}
+	return nil
+}
+
+// readKeyAndSaveToFolder will read file from ETCD and save into fileFolder
+func readKeyAndSaveToFolder(etcdKey, fileFolder string) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := etcdClient.Get(ctx, etcdKey, clientv3.WithPrefix())
+	cancel()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"etceKey":    etcdKey,
+			"fileFolder": fileFolder,
+			"err":        err,
+		}).Error("cannot read key ans save to folder")
+		return err
+	}
+	for _, ev := range resp.Kvs {
+		log.WithFields(log.Fields{
+			"etcdKey": string(ev.Key),
+		}).Info("read key")
+		filePath := filepath.Join(fileFolder, string(ev.Key))
+		saveToFolder(filePath, ev.Value)
+	}
+	return nil
+}
+
+func saveToFolder(filePath string, fileContent []byte) (err error) {
+	if err := ensureDir(filepath.Dir(filePath)); err != nil {
+		log.WithFields(log.Fields{
+			"filePath": filePath,
+			"err":      err,
+		}).Error("cannot create folder")
+		return err
+	}
+	if err := os.WriteFile(filePath, fileContent, 0644); err != nil {
+		log.WithFields(log.Fields{
+			"filePath": filePath,
+			"err":      err,
+		}).Error("cannot write file")
+		return err
+	}
+	return nil
+}
+
+// ensureDir will create folder if not exist
+func ensureDir(dirName string) error {
+	err := os.MkdirAll(dirName, os.ModePerm)
+	if err == nil {
+		return nil
+	}
+	if os.IsExist(err) {
+		// check that the existing path is a directory
+		info, err := os.Stat(dirName)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			return errors.New("path exists but is not a directory")
+		}
+		return nil
+	}
+	return err
+}
